dae31c47bedf5e5ec8a9db9a41f2fa1d8d8c8962
vanphuoc69 <81423784+vanphuocc@users.noreply.github.com>
2021-08-02 21:04:14 +0700

    fix loi

--- src/bai12_java_collection/bai_tap/bai1_product_management/ProductManager.java
+++ src/bai12_java_collection/bai_tap/bai1_product_management/ProductManager.java
@@ -3,7 +3,7 @@ package bai12_java_collection.bai_tap.bai1_product_management;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class ProductManager<E> {
+public class ProductManager {
     public static void add(ArrayList<Product> list){
         Scanner scanner=new Scanner(System.in);
         System.out.println("Nhập id sản phẩm");
@@ -13,17 +13,16 @@ public class ProductManager<E> {
         String inputName=scanner.nextLine();
         System.out.println("nhập giá sản phẩm");
         double inputPrice=scanner.nextInt();
-        Product product=new Product(inputId,inputName,inputPrice);
-        list.add(product);
+        list.add(new Product(inputId,inputName,inputPrice));
         display(list);
     }
     public static void remove(ArrayList<Product> list){
         System.out.println("nhập id sản phẩm muốn xóa");
         Scanner scanner=new Scanner(System.in);
         int inputId=scanner.nextInt();
-        for(Product product:list){
-            if(product.getId()==inputId){
-                list.remove(product);
+        for(int i=0;i<list.size();i++){
+            if(i==inputId){
+                list.remove(i-1);
             }
         }
         display(list);
@@ -49,12 +48,16 @@ public class ProductManager<E> {
         System.out.println("nhập tên sản phẩm bạn muốn tìm kiếm");
         Scanner scanner=new Scanner(System.in);
         String inputName=scanner.nextLine();
+        boolean check=false;
         for(Product product:list){
-            if(product.getName().equals(inputName)){
-                System.out.println("sản phẩm của bạn hiện đang có");
-            }else
-                System.out.println("sản phẩm không tồn tại");
+            if(product.getName().equals(inputName)) {
+                check = true;
+            }
         }
+        if(check){
+            System.out.println("sản phẩm có trong danh sách");
+        }else
+            System.out.println("sản phẩm không tồn tại");
     }
     public static void display(ArrayList<Product> list){
         for(Product product:list){
